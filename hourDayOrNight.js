ba0110041d9c53e7656cfcc6229a87bca807ecc6
Mariia Yesyp <mariia1yesyp@gmail.com>
2022-09-29 19:17:40 +0300

    added function hourDayOrNight1

--- hourDayOrNight.js
+++ hourDayOrNight.js
@@ -7,3 +7,16 @@ function hourDayOrNight(h) {
 }
 console.log(hourDayOrNight(5));
 console.log(hourDayOrNight(10));
+
+
+
+function hourDayOrNight1 (h) {
+    switch (h) {
+        case ( h >= 0 && h <= 0):
+            return "Have a good night!";
+        default: 
+            return "Have a good day!";
+    }
+}
+console.log(hourDayOrNight1(5));
+console.log(hourDayOrNight1(10));
